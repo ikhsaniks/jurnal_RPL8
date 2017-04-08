a17a25602819541b778ff53663ecb9668972ffda
ikhsaniks <miksya@gmail.com>
2017-04-08 11:50:00 +0700

    Update Jurnal8.java

--- Jurnal8.java
+++ Jurnal8.java
@@ -6,15 +6,15 @@ public class Jurnal8 {
 
     public static void main(String[] args) {
           int a=0,b=1,hasil=0,c;
-   Scanner input = new Scanner (System.in);
-   System.out.print ("Masukan Deret Fibonacci  : ");
-        c = input.nextInt ();
-        for(int i=0; i<=c; i++){            
-    a=b;
-    b=hasil;
-    System.out.print(hasil+" ");
-    hasil=a+b;
-    }
-    } 
+          Scanner input = new Scanner (System.in);
+          System.out.print ("Masukan Deret Fibonacci  : ");
+            c = input.nextInt ();
+            for(int i=0; i<=c; i++){            
+                a=b;
+                b=hasil;
+                System.out.print(hasil+" ");
+                hasil=a+b;
+            }
+        } 
     }
     
